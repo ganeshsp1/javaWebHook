60ed2e031b1b7fc30054f78c9c0b4424b92ddfba
Ganesh <ganesh.sp006@gmail.com>
2017-10-12 15:43:55 +0530

    image

---
+++ src/main/java/vo/Image.java
@@ -0,0 +1,33 @@
+package main.java.vo;
+public class Image
+{
+    private String accessibility_text;
+
+    private String url;
+
+    public String getAccessibility_text ()
+    {
+        return accessibility_text;
+    }
+
+    public void setAccessibility_text (String accessibility_text)
+    {
+        this.accessibility_text = accessibility_text;
+    }
+
+    public String getUrl ()
+    {
+        return url;
+    }
+
+    public void setUrl (String url)
+    {
+        this.url = url;
+    }
+
+    @Override
+    public String toString()
+    {
+        return "ClassPojo [accessibility_text = "+accessibility_text+", url = "+url+"]";
+    }
+}
