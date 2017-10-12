2c583458e7d663d21284af5e9d3829ab6ccb87bf
Ganesh <ganesh.sp006@gmail.com>
2017-10-12 14:54:04 +0530

    texttosp

--- src/main/java/hello/WebhookResponse.java
+++ src/main/java/hello/WebhookResponse.java
@@ -3,14 +3,19 @@ package hello;
 public class WebhookResponse {
     private final String speech;
     private final String displayText;
+    private final textToSpeech;
 
     private final String source = "java-webhook";
 
     public WebhookResponse(String speech, String displayText) {
         this.speech = speech;
         this.displayText = displayText;
+        this.textToSpeech = "texttoSpeech"
     }
-
+    public String getTextToSpeech() {
+        return textToSpeech;
+    }
+    
     public String getSpeech() {
         return speech;
     }
