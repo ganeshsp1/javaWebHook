f729d653775130d1606f4fa1f324a08c532ab2cb
Ganesh <ganesh.sp006@gmail.com>
2017-10-13 14:42:43 +0530

    abc

--- src/main/java/hello/HelloWorldController.java
+++ src/main/java/hello/HelloWorldController.java
@@ -1,10 +1,6 @@
 package hello;
 
 import main.java.vo.ApiAiRQ;
-import java.lang.reflect.Type;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -13,33 +9,13 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-import com.google.gson.JsonArray;
-import com.google.gson.JsonElement;
-import com.google.gson.JsonSerializationContext;
-import com.google.gson.JsonSerializer;
+import com.fasterxml.jackson.core.JsonGenerationException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 @Controller
 @RequestMapping("/webhook")
 public class HelloWorldController {
-	
-	static class CollectionAdapter implements JsonSerializer<Collection<?>> {
-		  @Override
-		  public JsonElement serialize(Collection<?> src, Type typeOfSrc,
-		    JsonSerializationContext context) {
-		   if (src == null || src.isEmpty())
-		    return null;
-
-		   JsonArray array = new JsonArray();
-
-		   for (Object child : src) {
-		    JsonElement element = context.serialize(child);
-		    array.add(element);
-		   }
-
-		   return array;
-		  }
-		 }
-	
 
     @RequestMapping(method = RequestMethod.POST)
     public @ResponseBody WebhookResponse webhook(@RequestBody String obj){
@@ -47,9 +23,18 @@ public class HelloWorldController {
         System.out.println(obj);
         obj=obj.replace("-", "");
         System.out.println(obj);
-        Gson gson = new GsonBuilder().registerTypeHierarchyAdapter(
-        	    Collection.class, new CollectionAdapter()).create();       
-        ApiAiRQ aiRQ= gson.fromJson(obj, ApiAiRQ.class);
-        return new WebhookResponse("Hello_fromSystem! "+  aiRQ.getStatus(), "Test" );
+//        Gson gson = new Gson();
+//        ApiAiRQ aiRQ= gson.fromJson(obj, ApiAiRQ.class);
+        ApiAiRQ aiRQ = new ApiAiRQ();
+        try {
+            ObjectMapper mapper = new ObjectMapper();
+        aiRQ = mapper.readValue(obj, ApiAiRQ.class);
+        
+        } catch (JsonGenerationException e) {
+			e.printStackTrace();
+		} catch (JsonMappingException e) {
+			e.printStackTrace();
+		}
+        return new WebhookResponse("Hello_fromSystem! "+  aiRQ.getStatus(), "Test" );        
     }
 }
