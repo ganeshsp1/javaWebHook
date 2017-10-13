8e1bd8d7b97be6a6ba79993509c5340b0d48ab1e
Ganesh <ganesh.sp006@gmail.com>
2017-10-13 14:27:56 +0530

    awsdfas

--- src/main/java/hello/HelloWorldController.java
+++ src/main/java/hello/HelloWorldController.java
@@ -1,6 +1,10 @@
 package hello;
 
 import main.java.vo.ApiAiRQ;
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -10,9 +14,32 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonSerializationContext;
+import com.google.gson.JsonSerializer;
 @Controller
 @RequestMapping("/webhook")
 public class HelloWorldController {
+	
+	static class CollectionAdapter implements JsonSerializer<Collection<?>> {
+		  @Override
+		  public JsonElement serialize(Collection<?> src, Type typeOfSrc,
+		    JsonSerializationContext context) {
+		   if (src == null || src.isEmpty())
+		    return null;
+
+		   JsonArray array = new JsonArray();
+
+		   for (Object child : src) {
+		    JsonElement element = context.serialize(child);
+		    array.add(element);
+		   }
+
+		   return array;
+		  }
+		 }
+	
 
     @RequestMapping(method = RequestMethod.POST)
     public @ResponseBody WebhookResponse webhook(@RequestBody String obj){
@@ -20,7 +47,8 @@ public class HelloWorldController {
         System.out.println(obj);
         obj=obj.replace("-", "");
         System.out.println(obj);
-        Gson gson = new GsonBuilder().serializeNulls().create();        
+        Gson gson = new GsonBuilder().registerTypeHierarchyAdapter(
+        	    Collection.class, new CollectionAdapter()).create();       
         ApiAiRQ aiRQ= gson.fromJson(obj, ApiAiRQ.class);
         return new WebhookResponse("Hello_fromSystem! "+  aiRQ.getStatus(), "Test" );
     }
