ee35e80b832850a761ffa541094e7e90829625da
ganeshsp1 <ganesh.sp006@gmail.com>
2018-05-06 06:52:02 +0530

    Create OriginalRequest.java

---
+++ src/main/java/vo/OriginalRequest.java
@@ -0,0 +1,44 @@
+public class OriginalRequest
+{
+    private String source;
+
+    private Data data;
+
+    private String version;
+
+    public String getSource ()
+    {
+        return source;
+    }
+
+    public void setSource (String source)
+    {
+        this.source = source;
+    }
+
+    public Data getData ()
+    {
+        return data;
+    }
+
+    public void setData (Data data)
+    {
+        this.data = data;
+    }
+
+    public String getVersion ()
+    {
+        return version;
+    }
+
+    public void setVersion (String version)
+    {
+        this.version = version;
+    }
+
+    @Override
+    public String toString()
+    {
+        return "ClassPojo [source = "+source+", data = "+data+", version = "+version+"]";
+    }
+}
