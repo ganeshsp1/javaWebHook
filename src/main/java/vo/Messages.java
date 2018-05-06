664a04cfd188c863351a9af73d2bdb1538172974
ganeshsp1 <ganesh.sp006@gmail.com>
2018-05-06 07:15:21 +0530

    Update Messages.java

--- src/main/java/vo/Messages.java
+++ src/main/java/vo/Messages.java
@@ -1,57 +1,9 @@
 package main.java.vo;
 public class Messages
 {
-    private String id;
-
     private String speech;
 
     private String type;
-    
-    private String platform;
-
-    private Items[] items;
-    
-    private String textToSpeech;
-    
-    public String getTextToSpeech ()
-    {
-        return textToSpeech;
-    }
-
-    public void setTextToSpeech (String textToSpeech)
-    {
-        this.textToSpeech = textToSpeech;
-    }
-    
-    public String getPlatform ()
-    {
-        return platform;
-    }
-
-    public void setPlatform (String platform)
-    {
-        this.platform = platform;
-    }
-
-    public Items[] getItems ()
-    {
-        return items;
-    }
-
-    public void setItems (Items[] items)
-    {
-        this.items = items;
-    }
-
-    public String getId ()
-    {
-        return id;
-    }
-
-    public void setId (String id)
-    {
-        this.id = id;
-    }
 
     public String getSpeech ()
     {
@@ -76,6 +28,7 @@ public class Messages
     @Override
     public String toString()
     {
-        return "ClassPojo [platform = "+platform+", items = "+items+",id = "+id+", speech = "+speech+", type = "+type+", textToSpeech = "+textToSpeech+"]";
+        return "ClassPojo [speech = "+speech+", type = "+type+"]";
     }
+}
 }
