241842c1e0a7bb323b5ad4d530f048b692220220
Ganesh <ganesh.sp006@gmail.com>
2018-05-06 08:36:30 +0530

    response

--- src/main/java/vo/Messages.java
+++ src/main/java/vo/Messages.java
@@ -1,9 +1,30 @@
-package vo;
-public class Messages
+package vo;public class Messages
 {
+    private String platform;
+
     private String speech;
 
+    private String title;
+
+    private Items[] items;
+
+    private String imageUrl;
+
+    private String subtitle;
+
+    private Buttons[] buttons;
+
     private String type;
+
+    public String getPlatform ()
+    {
+        return platform;
+    }
+
+    public void setPlatform (String platform)
+    {
+        this.platform = platform;
+    }
 
     public String getSpeech ()
     {
@@ -13,6 +34,56 @@ public class Messages
     public void setSpeech (String speech)
     {
         this.speech = speech;
+    }
+
+    public String getTitle ()
+    {
+        return title;
+    }
+
+    public void setTitle (String title)
+    {
+        this.title = title;
+    }
+
+    public Items[] getItems ()
+    {
+        return items;
+    }
+
+    public void setItems (Items[] items)
+    {
+        this.items = items;
+    }
+
+    public String getImageUrl ()
+    {
+        return imageUrl;
+    }
+
+    public void setImageUrl (String imageUrl)
+    {
+        this.imageUrl = imageUrl;
+    }
+
+    public String getSubtitle ()
+    {
+        return subtitle;
+    }
+
+    public void setSubtitle (String subtitle)
+    {
+        this.subtitle = subtitle;
+    }
+
+    public Buttons[] getButtons ()
+    {
+        return buttons;
+    }
+
+    public void setButtons (Buttons[] buttons)
+    {
+        this.buttons = buttons;
     }
 
     public String getType ()
@@ -28,6 +99,6 @@ public class Messages
     @Override
     public String toString()
     {
-        return "ClassPojo [speech = "+speech+", type = "+type+"]";
+        return "ClassPojo [platform = "+platform+", speech = "+speech+", title = "+title+", items = "+items+", imageUrl = "+imageUrl+", subtitle = "+subtitle+", buttons = "+buttons+", type = "+type+"]";
     }
-	}
+}
